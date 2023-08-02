026d4134fc6d6c0560b915426af3f8427c09cc93
Mrinmayi Kulkarni <mrinmayi@gra-login1.graham.sharcnet>
2023-08-01 22:36:23 -0400

    Added part and session inputs

--- fMRIPreprocessing/ReconstructDICOMs.sh
+++ fMRIPreprocessing/ReconstructDICOMs.sh
@@ -46,29 +46,29 @@ while ( $ac <= $#argv )
       echo ""
       echo "options specific to this script"
       echo ""
-	  echo "  -project PATH     : path to project folder that you're working on"
+      echo "  -project PATH     : path to project folder that you're working on"
       echo "  -input_dir DIR    : folder in project path where DICOM folders are stored"
-	  echo "						default: [project]/sourcedata"
+      echo "                        default: [project]/sourcedata"
       echo "  -output_dir DIR   : folder in project path where you want the NIFTIs"
-	  echo "						default: [project]/rawdata"
+      echo "                        default: [project]/rawdata"
       echo "  -session_name NAME: name of the session (if applicable)"
-	  echo "  -part_name NAME   : name of component of a complex representation of the MRI signal (e.g., SWI)."
-	  echo "						Note: see BIDS documentation for more information"
-	  echo "  -T1Data NUM    	: how many T1s you have"
-	  echo "						Note: make sure your T1 folders are called *T1w*"
-	  echo "						Note: T1s will be named [participant_id]_[T1_folder_name].nii"
-	  echo "  -T2Data NUM  		: how many T2s you have"
-	  echo "						Note: make sure your T2 folders are called *T2w*"
-	  echo "						Note: T2s will be named [participant_id]_[T2_folder_name].nii"
-  	  echo "  -fmap NUM  		: how many fieldmaps you have"
-	  echo "						Note: make sure your fmap folders are called *fmap*"
-	  echo "						Note: fmaps will be named [participant_id]_[fmap_folder_name].nii"
-	  echo "  -func NAME NUM    : name of the task you're converting followed by how many runs you have"
-	  echo "  -angioData NUM    : how many angiographs do you have"
-	  echo "						Note: make sure your angiography folders are called *angio*"
-	  echo "						Note: angiography scans will be named [participant_id]_[angio_folder_name].nii"
+      echo "  -part_name NAME   : name of component of a complex representation of the MRI signal (e.g., SWI)."
+      echo "                        Note: see BIDS documentation for more information"
+      echo "  -T1Data NUM    	: how many T1s you have"
+      echo "                        Note: make sure your T1 folders are called *T1w*"
+      echo "                        Note: T1s will be named [participant_id]_[T1_folder_name].nii"
+      echo "  -T2Data NUM        : how many T2s you have"
+      echo "                        Note: make sure your T2 folders are called *T2w*"
+      echo "                        Note: T2s will be named [participant_id]_[T2_folder_name].nii"
+      echo "  -fmap NUM          : how many fieldmaps you have"
+      echo "                        Note: make sure your fmap folders are called *fmap*"
+      echo "                        Note: fmaps will be named [participant_id]_[fmap_folder_name].nii"
+      echo "  -func NAME NUM     : name of the task you're converting followed by how many runs you have"
+      echo "  -angioData NUM     : how many angiographs do you have"
+      echo "                        Note: make sure your angiography folders are called *angio*"
+      echo "                        Note: angiography scans will be named [participant_id]_[angio_folder_name].nii"
       echo "  -dcm_string NAME  : string in DICOM directory that will be used for looking for dicoms"
-	  echo "						default: *dcm"
+      echo "                        default: *dcm"
       echo ""
       echo "general options"
       echo ""
@@ -115,7 +115,7 @@ while ( $ac <= $#argv )
       set part_name = part-$argv[$ac]
    else if ( "$argv[$ac]" == "-T1Data" ) then
       set T1Data = 1 #do T1
-	  @ ac ++
+      @ ac ++
       if ( $ac > $#argv ) then
          echo "** -T1Data: missing argument"
          exit 1
@@ -181,26 +181,26 @@ echo ""
 
 #Making sure all the paths exist
 if ( ! -d $project/$input_dir ) then
-	echo "**ERROR: $project/$input_dir does not exist!"
-	exit 1
+   echo "**ERROR: $project/$input_dir does not exist!"
+   exit 1
 endif
 set study_root_in = $project/$input_dir
 
 if ( ! -d $project/$output_dir ) then
-	echo "$project/$output_dir does not exist! Making it!"
-	mkdir -p $project/$output_dir
+   echo "$project/$output_dir does not exist! Making it!"
+   mkdir -p $project/$output_dir
 endif
 set study_root_out = $project/$output_dir
 
 #Check that participant list is provided
 if ( $subj_proc_list == all ) then
-	cd $study_root_in
-	set $subj_proc_list = ( sub-* )
+   cd $study_root_in
+   set $subj_proc_list = ( sub-* )
 else if ( $#subj_proc_list == 0 ) then
-	echo "**ERROR: no subject list provided"
-	echo "** if you want to do all subjects enter all"
-	echo "      and make sure folders are named sub-* in $study_root_in"
-	exit 1
+   echo "**ERROR: no subject list provided"
+   echo "** if you want to do all subjects enter all"
+   echo "      and make sure folders are named sub-* in $study_root_in"
+   exit 1
 endif
 echo "Made it here 1"
 #Setup some arrays so that you can loop over T1, T2, fmap reconstruction
@@ -212,105 +212,105 @@ set modoutpaths = ( anat anat fmap func angio )
 echo $angioData
 ###Actually start dealing with subjects
 foreach subj ( $subj_proc_list )
-	cd $study_root_in/$subj
-	echo "Made it here 2"
-	foreach modc ( `seq 1 $#moddo` )
-		#Did the user turn on this modality?
-		if ( $moddo[$modc] == 1 ) then
-			set modpaths = ` find $study_root_in/$subj/$session_name -type d -name "*$modnames[$modc]*" `
-			set modpaths = `echo $modpaths | fmt -1 | sort -n`
-			set outpath = $study_root_out/$subj/$session_name/$modoutpaths[$modc]
+   cd $study_root_in/$subj
+   echo "Made it here 2"
+   foreach modc ( `seq 1 $#moddo` )
+      #Did the user turn on this modality?
+      if ( $moddo[$modc] == 1 ) then
 
-			#Do they match up with how many are expected based on user-input?
-			if ( $#modpaths < $modnums[$modc] ) then
-				echo "\nWARNING: Only $#modpaths found. You indicated $modnums[$modc] for $modnames[$modc]."
-				echo "           Only the first $modnums[$modc] will be processed!\n"
-			endif
+         #Need to figure out if there's a session and part name to be added to the file name
+         if ( "$session_name" != "" ) then
+            set niiname_sessionlabel = _${session_name}
+         else
+            set niiname_sessionlabel = ""
+         endif
+         if ( "$part_name" != "" ) then
+            set niiname_partlabel = _${part_name}
+         else
+            set niiname_partlabel = ""
+         endif
 
-			#Set how much zero-padding needs to be done based on how many runs are available
-			#This is importantt to do because not having leading zeros messes up the order that
-			#files are listed in. E.g. run10, run11 will be listed before run1, run2 etc.
-			if ( $modnums[$modc] <= 9 ) then
-				set d = 0
-			else if ( $modnums[$modc] > 9 ) then
-				set d = 0
-			else if ( $modnums[$modc] > 99 ) then
-				set d = 00
-			endif
+         set modpaths = ` find $study_root_in/$subj/$session_name -type d -name "*$modnames[$modc]${niiname_partlabel}*" `
+         set modpaths = `echo $modpaths | fmt -1 | sort -n`
+         set outpath = $study_root_out/$subj/$session_name/$modoutpaths[$modc]
 
-			#Print out a warning for how files will be converted. Let the user make an informed decision
-			echo "\nWARNING: The files are sorted in this this way:"
-			echo ${modpaths[*]} | tr " " "\n"
-			echo "Make sure you don't need to zero-pad your filenames!"
+         #Do they match up with how many are expected based on user-input?
+         if ( $#modpaths < $modnums[$modc] ) then
+            echo "\nWARNING: Only $#modpaths found. You indicated $modnums[$modc] for $modnames[$modc]."
+            echo "           Only the first $modnums[$modc] will be processed!\n"
+         endif
 
-			#Go through each folder
-			foreach runnum ( `seq -w ${d}1 $modnums[$modc] ` ) #$modpaths
-				
-				echo "\n************************* Now on ${modnames[${modc}]}: run ${runnum} *************************" #
-				
-				set rundir = $modpaths[$runnum]
-				echo "Converting $rundir"
+         #Set how much zero-padding needs to be done based on how many runs are available
+         #This is importantt to do because not having leading zeros messes up the order that
+         #files are listed in. E.g. run10, run11 will be listed before run1, run2 etc.
+         if ( $modnums[$modc] <= 9 ) then
+            set d = 0
+         else if ( $modnums[$modc] > 9 ) then
+            set d = 0
+         else if ( $modnums[$modc] > 99 ) then
+            set d = 00
+         endif
 
-				#new scanner saves dcms 2 folders deep in the run folder, so look for the actual dcms
-				#if you give the whole path in find, it'll return the whole path
-				set dcmpath = `find $rundir -type f -name "*$dcm_string*" | head -n 1` #$study_root_in/$subj/$rundir
+         #Print out a warning for how files will be converted. Let the user make an informed decision
+         echo "\nWARNING: The files are sorted in this this way:"
+         echo ${modpaths[*]} | tr " " "\n"
+         echo "Make sure you don't need to zero-pad your filenames!"
+
+         #Go through each folder
+         foreach runnum ( `seq -w ${d}1 $modnums[$modc] ` ) #$modpaths
+            
+            echo "\n************************* Now on ${modnames[${modc}]}: run ${runnum} *************************" #
+            
+            set rundir = $modpaths[$runnum]
+            echo "Converting $rundir"
+
+            #new scanner saves dcms 2 folders deep in the run folder, so look for the actual dcms
+            #if you give the whole path in find, it'll return the whole path
+            set dcmpath = `find $rundir -type f -name "*$dcm_string*" | head -n 1` #$study_root_in/$subj/$rundir
                 #get just the name of the path until the folder
                 set dcmdir = `dirname $dcmpath`
-				#get just the name of the folder
-				set niidirname = `basename $rundir`
+            #get just the name of the folder
+            set niidirname = `basename $rundir`
 
-            #Need to figure out if there's a session and part name to be added to the file name
-				if ( "$session_name" != "" ) then
-					set niiname_sessionlabel = ${session_name}_
-				else
-					set niiname_sessionlabel = ""
-				endif
-				if ( "$part_name" != "" ) then
-					set niiname_partlabel = ${part_name}_
-				else
-					set niiname_partlabel = ""
-				endif
-
-
-				#Set name of NIFTI based on whether we are working with funcs or not
+            #Set name of NIFTI based on whether we are working with funcs or not
             if ( $modnames[$modc] == $funcName ) then
-               set nii_name = ${subj}_${niiname_sessionlabel}task-${funcName}_run-${runnum}_${niiname_partlabel}bold
+               set nii_name = ${subj}${niiname_sessionlabel}_task-${funcName}_run-${runnum}${niiname_partlabel}_bold
             else if ( $modnames[$modc] == fmap ) then
-               set nii_name = ${subj}_${niiname_sessionlabel}task-${niidirname}_run-${runnum}_${niiname_partlabel}bold
+               set nii_name = ${subj}${niiname_sessionlabel}_task-${niidirname}_run-${runnum}${niiname_partlabel}_bold
             else
-               set nii_name = ${subj}_${niiname_sessionlabel}run-${runnum}_${niiname_partlabel}$modnames[$modc]
-				endif
+               set nii_name = ${subj}${niiname_sessionlabel}_run-${runnum}${niiname_partlabel}_$modnames[$modc]
+            endif
 
-				echo "NIFTI will be called ${nii_name}.nii"
+            echo "NIFTI will be called ${nii_name}.nii"
 
-				cd $rundir
+            cd $rundir
 
-				cd $dcmdir
-				#Reconstruct if NIFTIs don't already exist
-				if ( -f  $outpath/${nii_name}.nii ) then
-					if ( $redo == 0 ) then 
-						echo "Warning: $outpath/${nii_name}.nii already exists! Not overwriting!"
-						echo "Use find $outpath/ -type f -name *.nii -delete to delete all NIFTIs in one go."
-						set convert = 0 #should dcm2niix be run?
-					else if ( $redo == 1 ) then
-						echo "Warning: $outpath/${nii_name}.nii already exists and redo set to 1! Deleting NIFTI!"
-						rm $outpath/${nii_name}.*
-						set convert = 1
-					endif
-				else if ( ! -f  $outpath/${nii_name}.nii ) then
-					if ( ! -d $outpath/ ) then
-						echo "$outpath/ does not exist! Making it!"
-						mkdir -p $outpath/
-					endif #check if output folder exists
-					set convert = 1
-				endif #check if NIFTIs already exist
-				if ( $convert == 1 ) then 
-					dcm2niix_afni -f ${nii_name} -o $outpath/ $dcmdir
-				endif #check if all the conditions are right to run dcm2niix
-				@ runnum ++ #increment run number
-			end #go through runs of modality
-		endif #check whether the user turned conversion of this modality on
-	end #go through all modalities
+            cd $dcmdir
+            #Reconstruct if NIFTIs don't already exist
+            if ( -f  $outpath/${nii_name}.nii ) then
+               if ( $redo == 0 ) then 
+                  echo "Warning: $outpath/${nii_name}.nii already exists! Not overwriting!"
+                  echo "Use find $outpath/ -type f -name *.nii -delete to delete all NIFTIs in one go."
+                  set convert = 0 #should dcm2niix be run?
+               else if ( $redo == 1 ) then
+                  echo "Warning: $outpath/${nii_name}.nii already exists and redo set to 1! Deleting NIFTI!"
+                  rm $outpath/${nii_name}.*
+                  set convert = 1
+               endif
+            else if ( ! -f  $outpath/${nii_name}.nii ) then
+               if ( ! -d $outpath/ ) then
+                  echo "$outpath/ does not exist! Making it!"
+                  mkdir -p $outpath/
+               endif #check if output folder exists
+               set convert = 1
+            endif #check if NIFTIs already exist
+            if ( $convert == 1 ) then 
+               dcm2niix_afni -f ${nii_name} -o $outpath/ $dcmdir
+            endif #check if all the conditions are right to run dcm2niix
+            @ runnum ++ #increment run number
+         end #go through runs of modality
+      endif #check whether the user turned conversion of this modality on
+   end #go through all modalities
 end #go through all subjects
 
 
